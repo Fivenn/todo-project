b943436b4447b6b6ecc6fb82d2260af859129052
Nicolas SUEUR <nicolas1.sueur@orange.com>
2020-03-27 09:06:12 +0100

    update controller of the todo rest api (need to fix update request)

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -52,13 +52,54 @@ exports.findOne = (req, res) => {
 };
 
 exports.update = (req, res) => {
-
+    if(!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty"
+        });
+    }
+    Todo.updateById(
+        req.params.todoId,
+        new Todo(req.body),
+        (err, data) => {
+            if(err) {
+                if(err.kind === "not_found") {
+                    res.status(404).send({
+                        message: `Not found Todo with id ${req.params.todoId}.`
+                    });
+                } else {
+                    res.status(500).send({
+                        message: "Error updating Todo with id " + req.params.todoId
+                    });
+                } 
+            } else {
+                res.send(data);
+            }
+        }
+    );
 };
 
 exports.delete = (req, res) => {
-
+    Todo.remove(req.params.todoId, (err, data) => {
+        if(err) {
+            if(err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Todo with id ${req.params.todoId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Could not delete Todo with id " + req.params.todoId
+                });
+            }
+        } else res.send({ message: `Todo was deleted successfully!` });
+    });
 };
 
 exports.deleteAll = (req, res) => {
-
+    Todo.removeAll((err, data) => {
+        if(err) {
+            res.status(500).send({
+                message: err.message || "Some error occured while removing all todos."
+            });
+        } else res.send({ message: `All Todos was deleted successfully!` });
+    });
 };
