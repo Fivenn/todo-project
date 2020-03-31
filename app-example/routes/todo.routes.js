1964d1a1fa978c846d16f477e68e5e7d68b83362
Nicolas SUEUR <nicolas1.sueur@orange.com>
2020-03-31 10:32:34 +0200

    - implement edit todo feature

--- app-example/routes/todo.routes.js
+++ app-example/routes/todo.routes.js
@@ -1,10 +1,12 @@
 module.exports = app => {
     const todo = require("../controllers/todo.controller.js"),
-    bodyParser = require('body-parser');
-    
+    editTodo = require("../controllers/todo.edit.controller.js"),
+    bodyParser = require('body-parser'),
     urlencodedParser = bodyParser.urlencoded({ extended: true });
 
     app.get('/todo', todo.getTodos);
     app.post('/todo/add', urlencodedParser, todo.addTodo);
     app.get('/todo/delete/:id', todo.deleteTodo);
+    app.get('/todo/edit/:id', editTodo.getTodo);
+    app.post('/todo/update/:id', urlencodedParser, editTodo.updateTodo);
 };
