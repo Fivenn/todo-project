a5566e027bea4361950d84f49e655e9877a44fd9
Nicolas SUEUR <nicolas1.sueur@orange.com>
2020-03-31 09:39:58 +0200

    - update app to implement status list

--- app-example/controllers/todo.controller.js
+++ app-example/controllers/todo.controller.js
@@ -1,17 +1,28 @@
 const axios = require('axios'),
 Todo = require('../models/todo.model.js');
+Status = require('../models/status.model.js');
 
 exports.getTodos = (req, res) => {
     req.session.todolist = [];
+    req.session.statuslist = [];
     axios.get('http://localhost:3000/todos/').then(function(response) {
         response.data.forEach(element => {
             const todo = new Todo(element);
             req.session.todolist.push(todo);
         });
+        return axios.get('http://localhost:3000/status/');
+    }).then(function(response) {
+        response.data.forEach(element => {
+            const status = new Status(element);
+            req.session.statuslist.push(status);
+        });
     }).catch(function(error) {
         console.log(error);
     }).then(function() {
-        res.render('todo.ejs', { todolist: req.session.todolist });
+        res.render('todo.ejs', { 
+            todolist: req.session.todolist,
+            statuslist: req.session.statuslist 
+        });
     })
 };
 
